abdecf166d1b07cf903406e80cda17a2f9aa921c
latha1307 <ldkannammal@gmail.com>
2026-05-20 07:33:47 +0530

    Successfully connected with MongoDB

--- auth-service/src/main/java/com/aurevia/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/aurevia/authservice/controller/AuthController.java
@@ -1,13 +1,27 @@
 package com.aurevia.authservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.aurevia.authservice.entity.User;
+import com.aurevia.authservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
-    
-    @GetMapping("/api/auth/test")
-    public String test() {
-        return "Auth Service is working!";
-    }
+
+    private final UserRepository userRepository;
+
+    @PostMapping("/create")
+public User createUser() {
+
+    User user = new User();
+
+    user.setName("Latha");
+    user.setEmail("ldkannammal@gmail.com");
+    user.setPassword("123456");
+    user.setRole("USER");
+
+    return userRepository.save(user);
+}
 }
---
+++ auth-service/src/main/java/com/aurevia/authservice/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.aurevia.authservice.repository;
+
+import com.aurevia.authservice.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+}
