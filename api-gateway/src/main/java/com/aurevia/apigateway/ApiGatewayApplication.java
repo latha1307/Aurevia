f81545d15e757e4d30f64770c39ae81b2ca7e2cc
latha1307 <ldkannammal@gmail.com>
2026-06-07 11:00:57 +0530

    fixing api gateway

--- api-gateway/src/main/java/com/aurevia/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/aurevia/apigateway/ApiGatewayApplication.java
@@ -1,15 +1,22 @@
 package com.aurevia.apigateway;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner runner(
+            @Value("${eureka.client.service-url.defaultZone}")
+            String url) {
+        return args -> System.out.println("EUREKA URL = " + url);
+    }
 }
